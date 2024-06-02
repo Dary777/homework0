def648fdf067c16900b26b90194b59f979aa23bc
Dary <dashnep07@gmail.com>
2024-06-02 07:36:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# №1
+a = 'i can do it with a broken heart'
+print(len(a))
+# №2&3
+first = 7
+second = 3
+summa = first + second
+diff = first - second
+print(summa)
+print(diff)
+third = 2
+mean = (first + second + third) / 3
+print(mean)
+# №4
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+# №5
+a = 4
+b = 5
+c = 6
+f = (a*b)+(a*c)
+g = (f**3)/2
+print(g)
